7dac1c68641dc8aa27bacc66618574799dad5645
Timon Baudry <tibaudry@overstock.com>
2019-08-09 10:14:54 +0100

    Added multipartFile conversion

--- src/main/java/com/ostk/workexperience/twittermachine/controller/TwitterController.java
+++ src/main/java/com/ostk/workexperience/twittermachine/controller/TwitterController.java
@@ -1,19 +1,22 @@
 package com.ostk.workexperience.twittermachine.controller;
 
-import javax.inject.Inject;
-
-import lombok.extern.log4j.Log4j2;
-import twitter4j.Status;
-
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
+import javax.inject.Inject;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ostk.workexperience.twittermachine.service.Twitter;
+
+import lombok.extern.log4j.Log4j2;
+import twitter4j.Status;
 
 @Log4j2
 @RestController
@@ -59,5 +62,16 @@ public class TwitterController {
     //TODO: Response from twitter needs to be stored and returned
     twitter.fetchTimeline();
     return ResponseEntity.ok().build();
+  }
+
+  private File multiToFile(final MultipartFile multipartFile) throws IOException {
+    final File file = new File(multipartFile.getOriginalFilename());
+    if(file.createNewFile()) {
+      try (FileOutputStream fos = new FileOutputStream(file)) {
+        fos.write(multipartFile.getBytes());
+      }
+      return file;
+    }
+    return null;
   }
 }
