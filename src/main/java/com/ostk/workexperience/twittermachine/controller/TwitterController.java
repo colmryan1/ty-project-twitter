eae3e3d39bf4d2608f9ada29a027e823d399718b
Timon Baudry <tibaudry@overstock.com>
2019-08-16 14:48:30 +0100

    CleanUp

--- src/main/java/com/ostk/workexperience/twittermachine/controller/TwitterController.java
+++ src/main/java/com/ostk/workexperience/twittermachine/controller/TwitterController.java
@@ -1,27 +1,15 @@
 package com.ostk.workexperience.twittermachine.controller;
 
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import javax.inject.Inject;
-
-import lombok.extern.log4j.Log4j2;
-import org.springframework.web.multipart.MultipartFile;
-import twitter4j.Status;
-
-import java.util.ArrayList;
-
-import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 import com.ostk.workexperience.twittermachine.service.Twitter;
+
+import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @RestController
@@ -31,10 +19,11 @@ public class TwitterController {
   @Inject
   private Twitter twitter;
 
-    @PostMapping(value = "tweet")
+  @PostMapping(value = "tweet")
   public ResponseEntity sendTweet(final String tweetMessage) {
     log.info("Message received: {}", tweetMessage);
-    twitter.publish(tweetMessage);
+    final String message = null;
+    twitter.publish(message);
     return ResponseEntity.ok("Message Successfully published");
   }
 
@@ -43,59 +32,4 @@ public class TwitterController {
     log.info("Message received: {}", message);
     return ResponseEntity.ok("you said: " + message);
   }
-
-  @PostMapping(value = "challenge")
-  public ResponseEntity sendTweet(final String[] messages){
-        int i;
-      log.info("Message received: {}", messages);
-      for (i= 0; i <= 5; i++) {
-          twitter.publish(messages[i]);
-      }
-
-    return ResponseEntity.ok("Message Successfully published");
-  }
-
-  @PostMapping(value = "search")
-  public ResponseEntity searchTweets(final String queryString) {
-    int i;
-    log.info("Searching for: {}", queryString);
-    final List<Status> tweets = twitter.search(queryString);// <- Something in there might be broken
-
-          List<String> filteredList= new ArrayList<> ();
-          for (Status entry : tweets) {
-               String john = entry.getText ();
-               filteredList.add (john);
-         }
-
-
-    return ResponseEntity.ok(filteredList);
-  }
-
-  @PostMapping(value = "timeline")
-  public ResponseEntity fetchTimeline() {
-    log.info("Fetching Timeline");
-    final List<Status> tweets = twitter.fetchTimeline();
-    return ResponseEntity.ok (tweets);
-  }
-
-
-    @PostMapping(value = "Upload Image")
-    public ResponseEntity uploadMedia(final MultipartFile josh, String message) throws IOException {
-        log.info("Uploading");
-        File newJosh = multiToFile (josh);
-        twitter.upload (newJosh, message);
-        return ResponseEntity.ok ("Josh has been uploaded");
-    }
-
-    private File multiToFile(final MultipartFile multipartFile) throws IOException {
-        final File file = new File(multipartFile.getOriginalFilename());
-        if(file.createNewFile()) {
-            try (FileOutputStream fos = new FileOutputStream(file)) {
-                fos.write(multipartFile.getBytes());
-            }
-            return file;
-        }
-        return null;
-
-    }
 }
--- src/main/java/com/ostk/workexperience/twittermachine/service/Twitter.java
+++ src/main/java/com/ostk/workexperience/twittermachine/service/Twitter.java
@@ -3,89 +3,26 @@ package com.ostk.workexperience.twittermachine.service;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-
 import lombok.extern.log4j.Log4j2;
-import twitter4j.*;
-
-
-import java.io.File;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Locale;
+import twitter4j.Status;
+import twitter4j.TwitterException;
 
 @Log4j2
 @Named
 public class Twitter implements ITwitter {
 
-    @Inject
-    private twitter4j.Twitter twitter;
+  @Inject
+  private twitter4j.Twitter twitter;
 
-      @Override
-      public void publish(final String message) {
-        final Status result;
-        try {
-          result = twitter.updateStatus(message);
-          log.info("Successfully published [{}]", result.getText());
-        }
-        catch (final TwitterException e) {
-          e.printStackTrace();
-        }
-      }
-
-      @Override
-      public List<Status> search(String queryString) {
-        final Query query = new Query(queryString);
-        query.setCount(5);
-        query.setResultType(Query.ResultType.popular);
-        query.setLocale(Locale.ENGLISH.getLanguage());
-        QueryResult result;
-        try {
-          result = twitter.search(query);
-          log.info("Successfully got {} tweets", result.getCount());
-          return result.getTweets();
-        }
-        catch (final TwitterException e) {
-          e.printStackTrace();
-          return Collections.emptyList();
-        }
-      }
-
-        @Override
-        public List<Status> fetchTimeline() {
-            Paging paging = new Paging ();
-            paging.setCount (5);
-            try {
-
-                ResponseList<Status> result = twitter.getHomeTimeline (paging);
-                log.info ("Successfully got timeline");
-                return result;
-            } catch (final TwitterException e) {
-                e.printStackTrace ();
-                return Collections.emptyList ();
-            }
-
-      }
-
-        @Override
-        public void upload(File timon, String caption) {
-          try {
-                UploadedMedia um = twitter.tweets ().uploadMedia (timon);
-                StatusUpdate su = new StatusUpdate (caption);
-                su.setMediaIds (um.getMediaId ());
-                twitter.tweets ().updateStatus (su);
-                log.info ("Successfully Uploaded Media File");
-            } catch (final TwitterException e) {
-                e.printStackTrace ();
-            }
-
-
-
-
-
-
-        }
-
+  @Override
+  public void publish(final String message) {
+    final Status result;
+    try {
+      result = twitter.updateStatus(message);
+      log.info("Successfully published [{}]", result.getText());
     }
-
-
+    catch (final TwitterException e) {
+      e.printStackTrace();
+    }
+  }
+}
