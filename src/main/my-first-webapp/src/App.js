c00575c82d7102baffeb126f9fe0ed754afaf8b5
Timon Baudry <tibaudry@overstock.com>
2019-08-16 15:23:30 +0100

    Added Basic react stuff

---
+++ src/main/my-first-webapp/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import MyUI from './myUI';
+
+function App() {
+  return (
+    <MyUI/>
+  );
+}
+
+export default App;
---
+++ src/main/my-first-webapp/src/myUI.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {publish} from './functions';
+
+class myUi extends Component {
+
+    constructor(props) {
+        super(props);
+        this.publishTweet = this.publishTweet.bind(this);
+    }
+
+    publishTweet() {
+        console.log('I have been called');
+        let text = document.getElementById('helloWorldHeader').innerHTML;
+        console.log('This is my text: ', text);
+        publish(text);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1 id='helloWorldHeader'>Hello World!</h1>
+                <input type='button' onClick={() => {this.publishTweet()}} value='I wont do anything'/>
+            </div>
+        );
+    }
+}
+
+export default myUi;
